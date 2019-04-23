a29143397e7718215541dfdf01a1d7328e81f4f1
Jidor Tang <tlqtangok@126.com>
2019-04-23 17:09:14 +0800

    - edit bld_sh.sh at 20190423_1709

--- bld_sh.sh
+++ bld_sh.sh
@@ -3,13 +3,15 @@ set -e
 #./configure --prefix=`pwd`/bld
 #make install 
 
-export ROOT_PB=/home/bbb902/t/t/git/protobuf-3.7.0/bin
+export ROOT_PB=/mnt/jd/t/git/protobuf-3.7.0/bld_out
 export PATH=$ROOT_PB/bin:$PATH
 export  PKG_CONFIG_PATH=$ROOT_PB/lib/pkgconfig   # NEVER add anything more here !!!
 export LD_LIBRARY_PATH=$ROOT_PB/lib:$LD_LIBRARY_PATH
 
 #protoc --cpp_out=. --java_out=. --python_out=. test.proto
 protoc --cpp_out=. test.proto
+
+# need run `sudo apt install pkg-config`
 g++ -std=c++11 main.cpp test.pb.cc -o main_test_proto.exe `pkg-config --cflags --libs protobuf`
 
 ./main_test_proto.exe 
